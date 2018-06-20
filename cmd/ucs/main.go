4f61cdbdae8138564db5640e5eb5d3e01ebc2a99
Morten Siebuhr <sbhr@sbhr.dk>
2018-06-20 21:46:58 +0200

    CLI: Allow selecting cacher backend

--- cmd/ucs/main.go
+++ cmd/ucs/main.go
@@ -4,17 +4,43 @@ import (
 	"context"
 
 	"gitlab.com/msiebuhr/ucs"
+	"gitlab.com/msiebuhr/ucs/cache"
 
 	"github.com/namsral/flag"
 )
 
-func main() {
-	var address string
-	flag.StringVar(&address, "address", ":8126", "Address and port to listen on")
+var (
+	cacheBackend string
+	address      string
+)
 
+func init() {
+	flag.StringVar(&cacheBackend, "cache-backend", "fs", "Cache backend (fs or memory)")
+	flag.StringVar(&address, "address", ":8126", "Address and port to listen on")
+}
+
+func main() {
 	flag.Parse()
 
-	server := ucs.NewServer()
+	// Figure out a cache
+	var c cache.Cacher
+	switch cacheBackend {
+	case "fs":
+		var err error
+		c, err = cache.NewFS()
+		if err != nil {
+			panic(err)
+		}
+	case "memory":
+		c = cache.NewMemory()
+	default:
+		// UNKNOWN BACKEND - BAIL/CRASH/QUIT
+		panic("Unknown backend " + cacheBackend)
+	}
+
+	server := ucs.NewServer(
+		func(s *ucs.Server) { s.Cache = c },
+	)
 
 	server.Listen(context.Background(), "tcp", address)
 }
