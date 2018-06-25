e938fbd497292ae4b51390526902c27bbf2077c3
Morten Siebuhr <sbhr@sbhr.dk>
2018-06-25 22:47:03 +0200

    Server: Multi-packet fuzzing + more samples

--- server_fuzz.go
+++ server_fuzz.go
@@ -1,7 +1,10 @@
 package ucs
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"io/ioutil"
 	"net"
 )
 
@@ -9,10 +12,19 @@ func Fuzz(data []byte) int {
 	client, server := net.Pipe()
 	s := NewServer()
 
+	// Send each line of data as a packet of it's own
 	go func() {
-		client.Write(data)
+		datas := bytes.Split(data, []byte("\n"))
+		for _, d := range datas {
+			client.Write(d)
+		}
+		client.Close()
 	}()
 
+	// Read but ignore returned data
+	go io.Copy(ioutil.Discard, client)
+
 	s.handleRequest(context.Background(), server)
+
 	return 0
 }
