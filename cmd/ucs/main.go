5c754373d0617bb0d291b34778ac1d44e693daef
Morten Siebuhr <sbhr@sbhr.dk>
2018-09-10 09:42:27 +0200

    cmd/ucs: Add humanized quota size arguments

--- cmd/ucs/main.go
+++ cmd/ucs/main.go
@@ -9,15 +9,50 @@ import (
 	"gitlab.com/msiebuhr/ucs"
 	"gitlab.com/msiebuhr/ucs/cache"
 
+	"github.com/docker/go-units"
 	"github.com/namsral/flag"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+// Quick-and-dirty human-readable sizes
+type Size struct {
+	size *int64
+}
+
+func (v Size) String() string {
+	if v.size == nil {
+		return ""
+	}
+	return units.HumanSize(float64(*v.size))
+}
+
+func (v Size) Int64() int64 {
+	if v.size == nil {
+		return 0
+	}
+	return *v.size
+}
+
+func (v Size) Set(s string) error {
+	b, err := units.FromHumanSize(s)
+	if err != nil {
+		return err
+	}
+	*v.size = b
+	return nil
+}
+
+func NewSize(s int64) *Size {
+	size := Size{}
+	size.size = &s
+	return &size
+}
 
 var (
 	cacheBackend string
 	address      string
 	HTTPAddress  string
-	quota        int64
+	quota        = NewSize(1e9)
 	verbose      bool
 )
 
@@ -25,24 +60,26 @@ func init() {
 	flag.StringVar(&cacheBackend, "cache-backend", "fs", "Cache backend (fs or memory)")
 	flag.StringVar(&address, "address", ":8126", "Address and port to listen on")
 	flag.StringVar(&HTTPAddress, "http-address", ":9126", "Address and port for HTTP metrics/admin interface")
-	flag.Int64Var(&quota, "quota", 1e9, "Storage quota in bytes")
 	flag.BoolVar(&verbose, "verbose", false, "Spew more info")
+	flag.Var(quota, "quota", "Storage quota (ex. 10GB, 1TB, ...)")
 }
 
 func main() {
 	flag.Parse()
+
+	log.Println("Starting. Quota ", quota)
 
 	// Figure out a cache
 	var c cache.Cacher
 	switch cacheBackend {
 	case "fs":
 		var err error
-		c, err = cache.NewFS(func(f *cache.FS) { f.Quota = quota })
+		c, err = cache.NewFS(func(f *cache.FS) { f.Quota = quota.Int64() })
 		if err != nil {
 			panic(err)
 		}
 	case "memory":
-		c = cache.NewMemory(quota)
+		c = cache.NewMemory(quota.Int64())
 	default:
 		// UNKNOWN BACKEND - BAIL/CRASH/QUIT
 		panic("Unknown backend " + cacheBackend)
