d372ba27e37764b4500c9e3c934986ef67677a70
Morten Siebuhr <sbhr@sbhr.dk>
2018-11-03 21:55:27 +0100

    Fmt: Fix missed go fmt'ing

--- customflags/flags_test.go
+++ customflags/flags_test.go
@@ -39,10 +39,10 @@ func TestSize(t *testing.T) {
 
 	s.Set("1KB")
 	if s.Int64() != 1024 {
-		t.Errorf("Expected '1K' => 1024 bytes, got %d", s.Int64());
+		t.Errorf("Expected '1K' => 1024 bytes, got %d", s.Int64())
 	}
 
 	if s.String() != "1KiB" {
-		t.Errorf("Expected '1KB'.String() => 1KiB, got %s", s.String());
+		t.Errorf("Expected '1KB'.String() => 1KiB, got %s", s.String())
 	}
 }
