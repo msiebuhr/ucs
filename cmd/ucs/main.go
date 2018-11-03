c3ba600e788687f76e4f8d874d9276ecc03f95b3
Morten Siebuhr <sbhr@sbhr.dk>
2018-11-03 22:16:52 +0100

    Frontend/ucs: First stab at showing dynamic info

--- cmd/ucs/main.go
+++ cmd/ucs/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -75,16 +77,60 @@ func main() {
 			func(s *ucs.Server) { s.Namespace = ns },
 		)
 		servers = append(servers, server)
-		go func() {
+		go func(port uint) {
 			err := server.Listen(context.Background(), fmt.Sprintf(":%d", port))
 			log.Fatalln("Listen:", err)
-		}()
+		}(port)
 	}
 
 	// Set up web-server mux
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 	mux.Handle("/", http.FileServer(frontend.FS(false)))
+	mux.HandleFunc("/api/info", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
+		// Figure out our IP
+		var ip net.IP
+		addrs, _ := net.InterfaceAddrs()
+		for _, addr := range addrs {
+			var i net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				i = v.IP
+			case *net.IPAddr:
+				i = v.IP
+			}
+			// Is this the right way to detect the IP?
+			if i.IsGlobalUnicast() {
+				ip = i
+				break
+			}
+		}
+
+		servers := map[string]string{}
+		for ns, port := range *ports {
+			// Parse address to figure out what port/ip we're bound to
+			tcpAddr := net.TCPAddr{
+				IP:   ip,
+				Port: int(port),
+			}
+			servers[ns] = tcpAddr.String()
+		}
+
+		data := struct {
+			QuotaBytes   int64
+			Servers      map[string]string
+			CacheBackend string
+		}{
+			QuotaBytes:   quota.Int64(),
+			Servers:      servers,
+			CacheBackend: cacheBackend,
+		}
+
+		e := json.NewEncoder(w)
+		e.Encode(data)
+	})
 
 	// Create the web-server itself
 	h := &http.Server{Addr: HTTPAddress, Handler: mux}
